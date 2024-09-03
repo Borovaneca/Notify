95256334b2c79a176ebb148996e9dd7403274e3d
Borovaneca <petyoveselinov@gmail.com>
2024-09-03 19:10:29 +0300

    Adjust path to pick a role gif.

--- src/main/java/bg/notify/listeners/WelcomeListener.java
+++ src/main/java/bg/notify/listeners/WelcomeListener.java
@@ -3,31 +3,34 @@ package bg.notify.listeners;
 import bg.notify.utils.EmbeddedMessages;
 import net.dv8tion.jda.api.entities.channel.concrete.PrivateChannel;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
-import net.dv8tion.jda.api.events.thread.member.ThreadMemberJoinEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.utils.FileUpload;
 import org.springframework.stereotype.Component;
 
-import java.io.File;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 @Component
 public class WelcomeListener extends ListenerAdapter {
-
-
 
     @Override
     public void onGuildMemberJoin(GuildMemberJoinEvent event) {
         if (event.getMember().getUser().isBot() || event.getMember().getUser().isSystem()) return;
 
         PrivateChannel privateChannel = event.getMember().getUser().openPrivateChannel().complete();
-        Path gifPath = Paths.get("src/main/resources/files/pick-a-role.gif");
-        File gifFile = gifPath.toFile();
 
-        privateChannel.sendMessageEmbeds(EmbeddedMessages.getWelcomeMessage())
-                .addFiles(FileUpload.fromData(gifFile, "pick-a-role.gif"))
-                .queue();
+        try {
+            InputStream gifInputStream = getClass().getClassLoader().getResourceAsStream("files/pick-a-role.gif");
+            if (gifInputStream == null) {
+                throw new FileNotFoundException("Resource not found: files/pick-a-role.gif");
+            }
+
+            privateChannel.sendMessageEmbeds(EmbeddedMessages.getWelcomeMessage())
+                    .addFiles(FileUpload.fromData(gifInputStream, "pick-a-role.gif"))
+                    .queue();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
