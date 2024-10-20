794294c457e610b00a21f5053ba0fecec7331a8c
Borovaneca <petyoveselinov@gmail.com>
2024-10-20 23:42:58 +0300

    Implemented Logger about lock/unlock events.

--- src/main/java/bg/notify/services/EventService.java
+++ src/main/java/bg/notify/services/EventService.java
@@ -1,6 +1,7 @@
 package bg.notify.services;
 
 import bg.notify.utils.EmbeddedMessages;
+import lombok.Setter;
 import net.dv8tion.jda.api.entities.channel.concrete.TextChannel;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
@@ -13,7 +14,9 @@ import java.util.concurrent.TimeUnit;
 
 import static bg.notify.constants.Constants.LOCK_CHANNELS_ENDPOINT;
 import static bg.notify.constants.Constants.UNLOCK_CHANNELS_ENDPOINT;
+import static org.slf4j.helpers.Reporter.info;
 
+@Setter
 @Component
 public class EventService {
 
@@ -27,21 +30,24 @@ public class EventService {
 
     private void proceed(String guildId, TextChannel logChannel, boolean closingEvent) throws IOException {
         String endPoint = closingEvent ? String.format(LOCK_CHANNELS_ENDPOINT, guildId) : String.format(UNLOCK_CHANNELS_ENDPOINT, guildId);
+        info("ENDPOINT - " + endPoint);
         Request request = new Request.Builder()
                 .url(endPoint)
                 .post(RequestBody.create(new byte[0]))
                 .build();
-
         OkHttpClient client = new OkHttpClient.Builder()
                 .connectTimeout(40, TimeUnit.SECONDS)
                 .writeTimeout(40, TimeUnit.SECONDS)
                 .readTimeout(3, TimeUnit.MINUTES)
                 .build();
+        info("Sending request.");
 
         try (Response response = client.newCall(request).execute()) {
             if (response.isSuccessful()) {
+                info("The response is: " + response.body().string());
                 logChannel.sendMessageEmbeds(EmbeddedMessages.getEventExecutedSuccessfully()).queue();
             } else {
+                info("The response is: " + response.body().string() + "\n Response Code: " + response.code());
                 logChannel.sendMessageEmbeds(EmbeddedMessages.getEventExecutedUnSuccessfully()).queue();
             }
         }
